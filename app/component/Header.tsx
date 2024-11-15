d51f767f1d799c27d80d16dc9e37dde919834a99
momoka <moowfaw@gmail.com>
2024-11-15 09:50:34 +0900

    fix: アイコンからPopoverを表示する

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -1,19 +1,39 @@
 import { auth, signIn, signOut } from '@/auth'
 import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 
 export const Header = async () => {
   const session = await auth()
   return (
     <header className="flex justify-end gap-4 p-4 shadow">
       {session ? (
-        <Button
-          onClick={async () => {
-            'use server'
-            await signOut()
-          }}
-        >
-          SignOut
-        </Button>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button size="icon" className="rounded-full">
+              <Avatar>
+                <AvatarImage
+                  src={`${session.user && session.user.image}`}
+                  alt={session.user && `${session.user.name}のアイコン`}
+                />
+              </Avatar>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <Button
+              onClick={async () => {
+                'use server'
+                await signOut()
+              }}
+            >
+              ログアウト
+            </Button>
+          </PopoverContent>
+        </Popover>
       ) : (
         <Button
           onClick={async () => {
