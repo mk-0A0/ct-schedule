991148d043599275b0cd5ffcc746e3c214f81fbe
momoka <moowfaw@gmail.com>
2024-11-16 15:03:04 +0900

    chore: popover => menubar

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -1,39 +1,41 @@
 import { auth, signIn, signOut } from '@/auth'
 import { Button } from '@/components/ui/button'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from '@/components/ui/menubar'
 
 export const Header = async () => {
   const session = await auth()
   return (
     <header className="flex justify-end gap-4 p-4 shadow">
       {session ? (
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button size="icon" className="rounded-full">
+        <Menubar className="rounded-full p-0 border-none">
+          <MenubarMenu>
+            <MenubarTrigger className="rounded-full p-0">
               <Avatar>
                 <AvatarImage
                   src={`${session.user && session.user.image}`}
                   alt={session.user && `${session.user.name}のアイコン`}
                 />
               </Avatar>
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent>
-            <Button
-              onClick={async () => {
-                'use server'
-                await signOut()
-              }}
-            >
-              ログアウト
-            </Button>
-          </PopoverContent>
-        </Popover>
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem
+                onClick={async () => {
+                  'use server'
+                  await signOut()
+                }}
+              >
+                ログアウト
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
       ) : (
         <Button
           onClick={async () => {
