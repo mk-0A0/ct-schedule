a9f4f7d22f72abc20f17f378435bb01111d400e5
momoka <moowfaw@gmail.com>
2025-03-25 09:45:29 +0900

    add: メンバーの配列からtableを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
 export default function Home() {
-  return <main></main>;
+  const members = ["😺", "🐶", "🐱", "🐭", "🐹"];
+
+  return (
+    <main>
+      <table>
+        <tbody>
+          <tr className="border-b">
+            <th className="border-r"></th>
+            {members.map((member, index) => (
+              <th key={`row-th-${index}`} className="w-10 h-10 border-r">
+                {member}
+              </th>
+            ))}
+          </tr>
+          {members.map((member, index) => (
+            <tr key={`col-tr-${index}`} className="border-b">
+              <th className="w-10 h-10 border-r">{member}</th>
+              {members.map((_, index) => (
+                <td key={`td-${index}`} className="w-10 h-10 border-r"></td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
 }
