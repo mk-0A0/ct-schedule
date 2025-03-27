1d9ccadfab0559d49e9593db6ad4168cef2a0728
momoka <moowfaw@gmail.com>
2025-03-28 08:43:31 +0900

    chore: コメント追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
                       trIndex - 1 === tdIndex && "bg-gray-100"
                     }`}
                   >
+                    {/* trIndexはmembersWithEmptyから取得しており、要素数が1つ多いため-1をしている */}
                     {(tdIndex + (trIndex - 1)) % members.length}
                   </td>
                 )
