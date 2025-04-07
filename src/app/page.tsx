94f2628caf37a784933708e8a8cd15488049ef6f
momoka <moowfaw@gmail.com>
2025-04-08 02:18:48 +0900

    chore: 変数名・key名の修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,23 +26,23 @@ export default function Home() {
     <main className="flex gap-10 justify-center mt-10">
       <table className="border-t border-l h-full">
         <tbody>
-          {membersWithEmpty.map((colMember, trIndex) => (
-            <tr key={`col-tr-${trIndex}`} className="border-b">
+          {membersWithEmpty.map((colMember, rowIndex) => (
+            <tr key={`tr-${rowIndex}`} className="border-b">
               <th className="w-10 h-10 border-r">{colMember}</th>
-              {members.map((rowMember, tdIndex) =>
-                trIndex === 0 ? (
-                  <th key={`row-th-${tdIndex}`} className="w-10 h-10 border-r">
+              {members.map((rowMember, colIndex) =>
+                rowIndex === 0 ? (
+                  <th key={`cell-th-${colIndex}`} className="w-10 h-10 border-r">
                     {rowMember}
                   </th>
                 ) : (
                   <td
-                    key={`td-${tdIndex}`}
+                    key={`cell-${colIndex}`}
                     className={`w-10 h-10 border-r text-center ${
-                      trIndex - 1 === tdIndex && "bg-gray-100"
+                      rowIndex - 1 === colIndex && "bg-gray-100"
                     }`}
                   >
-                    {/* trIndexはmembersWithEmptyから取得しており、要素数が1つ多いため-1をしている */}
-                    {(tdIndex + trIndex - 1) % members.length}
+                    {/* rowIndexはmembersWithEmptyから取得しており、要素数が1つ多いため-1をしている */}
+                    {(colIndex + rowIndex - 1) % members.length}
                   </td>
                 )
               )}
