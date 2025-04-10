3c089b5bbc96f291eddf75195b1e65d5db059c56
momoka <moowfaw@gmail.com>
2025-04-10 09:23:39 +0900

    chore: sectionでwrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,47 +23,49 @@ export default function Home() {
     .map((date) => format(date, "yyyy/M/d(E)", { locale: ja }));
 
   return (
-    <main className="flex gap-10 justify-center mt-10">
-      <table className="border-t border-l h-full">
-        <tbody>
-          {membersWithEmpty.map((colMember, rowIndex) => (
-            <tr key={`tr-${rowIndex}`} className="border-b">
-              <th className="w-10 h-10 border-r">{colMember}</th>
-              {members.map((rowMember, colIndex) =>
-                rowIndex === 0 ? (
-                  <th
-                    key={`cell-th-${colIndex}`}
-                    className="w-10 h-10 border-r"
-                  >
-                    {rowMember}
-                  </th>
-                ) : (
-                  <td
-                    key={`cell-${colIndex}`}
-                    className={`w-10 h-10 border-r text-center ${
-                      rowIndex - 1 === colIndex && "bg-gray-100"
-                    }`}
-                  >
-                    {/* rowIndexはmembersWithEmptyから取得しており、要素数が1つ多いため-1をしている */}
-                    {(colIndex + rowIndex - 1) % members.length}
-                  </td>
-                )
-              )}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <ul>
-        {mondays.map(
-          (monday, index) =>
-            index < members.length && (
-              <li key={monday}>
-                <span>{index % members.length}:</span>
-                <time dateTime={monday}>{monday}</time>
-              </li>
-            )
-        )}
-      </ul>
+    <main>
+      <section className="flex gap-10 justify-center mt-10">
+        <table className="border-t border-l h-full">
+          <tbody>
+            {membersWithEmpty.map((colMember, rowIndex) => (
+              <tr key={`tr-${rowIndex}`} className="border-b">
+                <th className="w-10 h-10 border-r">{colMember}</th>
+                {members.map((rowMember, colIndex) =>
+                  rowIndex === 0 ? (
+                    <th
+                      key={`cell-th-${colIndex}`}
+                      className="w-10 h-10 border-r"
+                    >
+                      {rowMember}
+                    </th>
+                  ) : (
+                    <td
+                      key={`cell-${colIndex}`}
+                      className={`w-10 h-10 border-r text-center ${
+                        rowIndex - 1 === colIndex && "bg-gray-100"
+                      }`}
+                    >
+                      {/* rowIndexはmembersWithEmptyから取得しており、要素数が1つ多いため-1をしている */}
+                      {(colIndex + rowIndex - 1) % members.length}
+                    </td>
+                  )
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <ul>
+          {mondays.map(
+            (monday, index) =>
+              index < members.length && (
+                <li key={monday}>
+                  <span>{index % members.length}:</span>
+                  <time dateTime={monday}>{monday}</time>
+                </li>
+              )
+          )}
+        </ul>
+      </section>
     </main>
   );
 }
