21924d21b725700d031f9c365dd64bbc21c6caf7
Momoka Kamijo <moowfaw@gmail.com>
2025-07-31 08:54:40 +0900

    fix: レイアウト修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,48 +44,59 @@ export default async function Home() {
     .map((date) => format(date, "yyyy/M/d(E)", { locale: ja }));
 
   return (
-    <main className="flex gap-10 justify-center mt-10">
-      <table className="border-t border-l h-full">
-        <tbody>
-          {/* "": 左上の空マス */}
-          {[{ name: "" }, ...memberData.members].map((colMember, rowIndex) => (
-            <tr key={`tr-${rowIndex}`} className="border-b">
-              <th className="w-10 h-10 border-r">{colMember.name}</th>
-              {memberData.members.map((rowMember, colIndex) =>
-                rowIndex === 0 ? (
-                  <th
-                    key={`cell-th-${colIndex}`}
-                    className="w-10 h-10 border-r"
-                  >
-                    {rowMember.name}
-                  </th>
-                ) : (
-                  <td
-                    key={`cell-${colIndex}`}
-                    className={`w-10 h-10 border-r text-center ${
-                      rowIndex - 1 === colIndex && "bg-gray-100"
-                    }`}
-                  >
-                    {/* rowIndexは空マス分要素数が1つ多いため-1をしている */}
-                    {(colIndex + rowIndex - 1) % memberData.members.length}
-                  </td>
-                )
-              )}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <ul>
-        {mondays.map(
-          (monday, index) =>
-            index < memberData.members.length && (
-              <li key={monday}>
-                <span>{index % memberData.members.length}:</span>
-                <time dateTime={monday}>{monday}</time>
-              </li>
-            )
-        )}
-      </ul>
+    <main className="max-w-7xl mx-auto p-10">
+      <p>現在の参加者：{memberData.members.length}人</p>
+      <p className="text-gray-500 text-sm">
+        ・参加人数が奇数： グレーアウトしたマスがお休み
+      </p>
+      <p className="text-gray-500 text-sm">
+        ・参加人数が偶数： 自分と数字・背景色が同じマスのメンバーとペアになる
+      </p>
+      <article className="flex gap-10 mt-10">
+        <table className="border-t border-l h-full">
+          <tbody>
+            {/* "": 左上の空マス */}
+            {[{ name: "" }, ...memberData.members].map(
+              (colMember, rowIndex) => (
+                <tr key={`tr-${rowIndex}`} className="border-b">
+                  <th className="w-10 h-10 border-r">{colMember.name}</th>
+                  {memberData.members.map((rowMember, colIndex) =>
+                    rowIndex === 0 ? (
+                      <th
+                        key={`cell-th-${colIndex}`}
+                        className="w-10 h-10 border-r"
+                      >
+                        {rowMember.name}
+                      </th>
+                    ) : (
+                      <td
+                        key={`cell-${colIndex}`}
+                        className={`w-10 h-10 border-r text-center ${
+                          rowIndex - 1 === colIndex && "bg-gray-100"
+                        }`}
+                      >
+                        {/* rowIndexは空マス分要素数が1つ多いため-1をしている */}
+                        {(colIndex + rowIndex - 1) % memberData.members.length}
+                      </td>
+                    )
+                  )}
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+        <ul>
+          {mondays.map(
+            (monday, index) =>
+              index < memberData.members.length && (
+                <li key={monday}>
+                  <span>{index % memberData.members.length}:</span>
+                  <time dateTime={monday}>{monday}</time>
+                </li>
+              )
+          )}
+        </ul>
+      </article>
     </main>
   );
 }
