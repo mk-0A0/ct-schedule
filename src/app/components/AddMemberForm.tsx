5fc4a9ae501e05e5d66b7385e43c869bf12c8fe5
Momoka Kamijo <moowfaw@gmail.com>
2025-08-29 19:10:22 +0900

    fix: inputのid名を変更

--- src/app/components/AddMemberForm.tsx
+++ src/app/components/AddMemberForm.tsx
@@ -4,12 +4,17 @@ export const AddMemberForm = () => {
   return (
     <form>
       <div>
-        <label htmlFor="member-name">名前</label>
-        <input type="text" id="member-name" />
+        <label htmlFor="name">名前</label>
+        <input name="name" type="text" id="name" required />
       </div>
       <div>
-        <input type="checkbox" id="member-participate" />
-        <label htmlFor="member-participate">CT参加メンバーとして追加</label>
+        <input
+          name="participate"
+          type="checkbox"
+          id="participate"
+          defaultChecked
+        />
+        <label htmlFor="participate">CT参加メンバーとして追加</label>
       </div>
       <button type="submit">追加</button>
     </form>
