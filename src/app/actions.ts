033846f8fd4eb83e2cd528b2312d3708a955dfbe
Momoka Kamijo <moowfaw@gmail.com>
2025-08-29 19:15:08 +0900

    feat: フォームの入力内容をDBに追加するaddMember関数の作成

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,23 @@
 "use server";
 import { neon } from "@neondatabase/serverless";
 
+const sql = neon(`${process.env.DATABASE_URL}`);
+
 export async function getMember() {
-  const sql = neon(`${process.env.DATABASE_URL}`);
   const data = await sql`SELECT * FROM public.member`;
   return data;
+}
+
+// メンバーの追加
+export async function addMember(formData: FormData) {
+  const name = formData.get("name") as string;
+  const participate = formData.get("participate") === "on";
+
+  const result = await sql`
+    INSERT INTO member (name, participate)
+    VALUES (${name}, ${participate})
+    RETURNING *;
+  `;
+
+  return result[0];
 }
