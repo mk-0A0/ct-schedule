0a5bd9ed1edee35ea292495c83c70f6c2cdc0355
Momoka Kamijo <moowfaw@gmail.com>
2025-08-31 19:55:32 +0900

    fix: 個別で編集する

--- src/app/components/EditMemberForm.tsx
+++ src/app/components/EditMemberForm.tsx
@@ -6,9 +6,9 @@ import { Checkbox } from "@/app/components/ui/checkbox";
 import { Input } from "@/app/components/ui/input";
 import { Label } from "@/app/components/ui/label";
 import { PencilLine } from "lucide-react";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
-export const EditMemberForm = ({ members }: { members: Member[] }) => {
+const EditMemberRow = ({ member }: { member: Member }) => {
   const [edit, setEdit] = useState(false);
 
   return (
@@ -17,34 +17,34 @@ export const EditMemberForm = ({ members }: { members: Member[] }) => {
         <PencilLine />
       </Button>
       {edit ? (
-        <form action="">
-          {members.map((member) => (
-            <Fragment key={member.id}>
-              <Input
-                name="name"
-                id="name"
-                key={member.id}
-                value={member.name}
-                onChange={() => {}}
-              />
-              <Checkbox
-                name="participate"
-                id="participate"
-                className="inline-block ml-2"
-                checked={member.participate}
-                onChange={() => {}}
-              />
-              <Label htmlFor="participate">CTに参加する</Label>
-            </Fragment>
-          ))}
+        <form action="" key={member.id}>
+          <Input
+            name="name"
+            id="name"
+            key={member.id}
+            value={member.name}
+            onChange={() => {}}
+          />
+          <Checkbox
+            name="participate"
+            id="participate"
+            className="inline-block ml-2"
+            checked={member.participate}
+            onChange={() => {}}
+          />
+          <Label htmlFor="participate">CTに参加する</Label>
         </form>
       ) : (
-        members.map((member) => (
-          <p key={member.id}>
-            {member.name}: {String(member.participate)}
-          </p>
-        ))
+        <p key={member.id}>
+          {member.name}: {String(member.participate)}
+        </p>
       )}
     </div>
   );
+};
+
+export const EditMemberForm = ({ members }: { members: Member[] }) => {
+  return members.map((member) => (
+    <EditMemberRow key={member.id} member={member} />
+  ));
 };
