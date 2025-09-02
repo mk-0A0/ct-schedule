54423c6f1cb88d54aee021b08de00aef2638f695
Momoka Kamijo <moowfaw@gmail.com>
2025-09-03 08:56:24 +0900

    fix: member一覧の余白調整

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -4,11 +4,13 @@ import { EditMemberForm } from "@/app/components/EditMemberForm";
 export default async function Home() {
   const memberData = await getMember();
   return (
-    <main className="max-w-7xl mx-auto p-10">
+    <main className="grid gap-5 max-w-7xl mx-auto p-10">
       <h1 className="text-2xl font-bold">メンバー一覧</h1>
-      {memberData.map((member) => (
-        <EditMemberForm key={member.id} member={member} />
-      ))}
+      <section className="grid gap-3">
+        {memberData.map((member) => (
+          <EditMemberForm key={member.id} member={member} />
+        ))}
+      </section>
     </main>
   );
 }
