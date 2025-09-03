f6812472208d588edbcd92f57753602783adf744
Momoka Kamijo <moowfaw@gmail.com>
2025-09-03 09:40:39 +0900

    fix: メンバー一覧にAddMemberFormDialogを追加

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -1,4 +1,5 @@
 import { getMember } from "@/app/actions";
+import { AddMemberFormDialog } from "@/app/components/AddMemberFormDialog";
 import { EditMemberForm } from "@/app/components/EditMemberForm";
 import { Separator } from "@/app/components/ui/separator";
 import { Fragment } from "react";
@@ -7,11 +8,14 @@ export default async function Home() {
   const memberData = await getMember();
   return (
     <main className="grid gap-5 max-w-7xl mx-auto p-10">
-      <h1 className="text-2xl font-bold">メンバー一覧</h1>
+      <div className="flex justify-between items-center gap-10">
+        <h1 className="text-2xl font-bold">メンバー一覧</h1>
+        <AddMemberFormDialog />
+      </div>
       <section className="grid gap-3">
         {memberData.map((member) => (
           <Fragment key={member.id}>
-            <EditMemberForm key={member.id} member={member} />
+          <EditMemberForm key={member.id} member={member} />
             <Separator />
           </Fragment>
         ))}
