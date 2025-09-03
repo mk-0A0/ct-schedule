3633716e5f011628518680a19bd5d02807f7aac2
Momoka Kamijo <moowfaw@gmail.com>
2025-09-03 09:56:00 +0900

    feat: DeleteMemberFormDialogコンポーネント作成

---
+++ src/app/components/DeleteMemberFormDialog.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { addMember } from "@/app/actions";
+import { useRouter } from "next/navigation";
+import { Button } from "@/app/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/components/ui/dialog";
+import { toast } from "sonner";
+import { Trash2 } from "lucide-react";
+
+export const DeleteMemberFormDialog = () => {
+  const router = useRouter();
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      toast.success(`${member.name} を削除しました`);
+      router.refresh();
+    } catch (error) {
+      toast.error("メンバーの削除に失敗しました");
+      console.error("Error adding member:", error);
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Trash2 />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <form action={handleSubmit} className="grid gap-5">
+          <DialogHeader>
+            <DialogTitle>メンバーを削除</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-2">${addMember.name} を削除しますか？</div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">キャンセル</Button>
+            </DialogClose>
+            <Button type="submit" variant="destructive">
+              削除
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
