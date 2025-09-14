bb751d9ae6bb3d46af6413fd195f10fe6a57871e
Momoka Kamijo <moowfaw@gmail.com>
2025-09-14 14:04:21 +0900

    feat: Slackのprovider情報追加

--- auth.ts
+++ auth.ts
@@ -2,5 +2,11 @@ import NextAuth from "next-auth";
 import Slack from "next-auth/providers/slack";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Slack],
+  providers: [
+    Slack({
+      clientId: process.env.SLACK_CLIENT_ID!,
+      clientSecret: process.env.SLACK_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
 });
